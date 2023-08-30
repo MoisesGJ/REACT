7dc0e85286bdd0bbe68a6c070fa61704060ce529
MoisesGJ <mgarciaj1802@alumno.ipn.mx>
2023-08-29 21:07:44 -0600

    Sess2

--- Sesss/practice-on-react/src/App.jsx
+++ Sesss/practice-on-react/src/App.jsx
@@ -1,10 +1,42 @@
-import './App.css';
-import MentorCard from './Components/MentorCard';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//[(user, setUser)] = useState();
 
 function App() {
+  const [text, setText] = useState('Hola');
+  const [user, setUser] = useState({});
+
+  const textHandler = ({ target }) => {
+    const value = target.value;
+
+    setText(value);
+  };
+
+  const userHandler = ({ target }) => {
+    const name = target.name;
+    const value = target.value;
+
+    setUser({ ...user, [name]: value });
+  };
+
   return (
     <>
-      <MentorCard name="Israel Salinas" description="Soy mentor de Kodemia" />
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        onChange={(event) => userHandler(event)}
+      />
+      <input
+        type="text"
+        placeholder="Lastname"
+        name="lastname"
+        onChange={(event) => userHandler(event)}
+      />
+
+      <h2>Name: {user.name}</h2>
+      <h2>Lastname: {user.lastname}</h2>
     </>
   );
 }
